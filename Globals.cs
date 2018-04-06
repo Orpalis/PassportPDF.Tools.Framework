6ff1988c18a9a2bd2fec3177c8c31944e989053c
bzglm <evan.carrere@epitech.eu>
2018-04-06 12:43:51 -0700

    Remove unnecessary using

--- Globals.cs
+++ Globals.cs
@@ -16,7 +16,6 @@
  *
  **********************************************************************/
 
-using System.Reflection;
 using System.Threading;
 using Orpalis.Globals.Localization;
 using PassportPDF.Model;
